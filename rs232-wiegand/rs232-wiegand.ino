214e797a14517f8564ed87909928abfe9cd81284
leonellopezvazquez <llopez@neology.mx>
2019-07-18 12:51:05 -0500

    envio por puerto serial de datos

--- rs232-wiegand/rs232-wiegand.ino
+++ rs232-wiegand/rs232-wiegand.ino
@@ -17,14 +17,15 @@ void setup() {
   Serial.begin(9600);
   Serial1.begin(9600);
   Serial2.begin(9600);
+  Serial3.begin(9600);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //receive all characteers in serial
-  if(Serial.available()){
-    char byte1 = Serial.read();
+  if(Serial1.available()){
+    char byte1 = Serial1.read();
     SerialFlagDone = 0;
     rs232_counter = RS232_WAIT_TIME;
     databytes[bytecount] = byte1;
@@ -39,20 +40,46 @@ void loop() {
 
     if(bytecount>0 && SerialFlagDone){
       
-      String AllInputData = "";
+      int flag =  Send_to_Controller();
+            
+      }
+    
+  
+}
+
+
+int Send_to_Controller(){
+  String AllInputData = "";
       for(int x = 0; x<bytecount;x++){
         AllInputData+= databytes[x];    
+        }
+
+      if (AllInputData==""){
+        return 0;
+        
         }
         
       int  ind1=AllInputData.indexOf('|');
       String antenna = AllInputData.substring(0,ind1);  
       int ind2=AllInputData.indexOf('|',ind1+1);
       String data = AllInputData.substring(ind1+1,ind2); 
+
+      ///depending of the antenna mux, send it to each controller
+     
+      if(antenna=="1"){
+          for(int x =0; x<data.length();x++){
+            Serial2.write(data[x]);  
+          }
+        }
+
+      if(antenna=="2"){
+        for(int x =0; x<data.length();x++){
+            Serial3.write(data[x]);  
+          }
+        }
       
       bytecount = 0;
       SerialFlagDone=0;
-            
-      }
-    
-  
-}
+      
+      return 1;
+  }
